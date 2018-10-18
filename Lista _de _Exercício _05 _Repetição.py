922dd770a35575836be782827f24ff434251afab
efersonnunes <30432865+efersonnunes@users.noreply.github.com>
2018-10-17 22:53:33 -0300

    Update Lista _de _Exercício _05 _Repetição.py

--- Lista _de _Exercício _05 _Repetição.py
+++ Lista _de _Exercício _05 _Repetição.py
@@ -34,4 +34,43 @@ for i in range (99):
         menor = numero
 print('Maior Número: ',maior,'\nMenor Número :',menor)
     
+## Questão 006 ##
+while True:
+    senha = 12345678
+    entrada = int(input('Senha:'))
+    if senha == entrada:
+        print('Correto')
+        break
+    else:
+        print('Incorreta')
+## Questão 007 ## 
+n = int(input('N° :'))
+n = n + 1
+fat = 1
+while n > 1:
+    n = n - 1
+    fat = fat * n
+print(f'Fatorial  é {fat}')
+
+## Questão 008
+
+while True:
+    mat = int(input('Matricula :'))
+    if mat == 0:
+        break
+    nome = input('Nome:')
+    soma = 0
+    for i in range (2):
+        nota = int(input('Nota :'))
+        soma = soma + nota
+        media = soma / 2
+        if media > 7:
+            resultado = 'Aprovado'
+        else:
+            resultado = 'Reprovado'
+    print('==' * 20)
+    print(f'Matricula {mat}\nNome: {nome}\nNotas: {nota}\nResultado : {resultado} ')
+    print('==' * 20)
+
+    
     
