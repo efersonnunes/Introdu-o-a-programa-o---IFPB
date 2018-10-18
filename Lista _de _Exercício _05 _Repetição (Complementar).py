c13edebbf16adeeb572638400afe96efb1550303
efersonnunes <30432865+efersonnunes@users.noreply.github.com>
2018-10-18 14:56:14 -0300

    Update Lista _de _Exercício _05 _Repetição (Complementar).py

--- Lista _de _Exercício _05 _Repetição (Complementar).py
+++ Lista _de _Exercício _05 _Repetição (Complementar).py
@@ -1,4 +1,4 @@
-#Questão 001-
+#Questão 001->
 print('''    1 - SAUDAÇÃO
     2 - BRONCA
     3- FELICITAÇÃO
@@ -15,3 +15,35 @@ while opcao != 0:
     opcao = int(input('Opcão :'))
     
     
+#Questão 002 ->
+while True:
+    num = int(input('Número :'))
+    if num % 2 == 0:
+        print('Par')
+    else:
+        print('Ímpar')
+    opcao = input('Continuar: S/N').upper()
+    if opcao == 'N':
+        break
+        
+     # Questão 003 - 
+    temperatura = int(input('Temperatura :'))
+data = (input('Dia: '))
+diaFrio = data
+diaQuente = data
+tempMaior = temperatura
+tempMenor = temperatura
+for i in range(2):
+    temperatura = int(input('Temperatura'))
+    data = (input('Dia: '))
+    if temperatura > tempMaior:
+        tempMaior = temperatura
+        diaQuente = data
+    if temperatura < tempMenor:
+        tempMenor = temperatura
+        diaFrio = data
+print('== * 20')
+print(f'Tem Mais Quente : {tempMaior} -> Dia  - {diaQuente} de Abril  \nTemp Mais Fria : {tempMenor} ->  Dia - {diaFrio} de Abril')
+    
+    
+  
